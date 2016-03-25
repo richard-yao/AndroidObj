b5e018351eb77b8d8e174b1a5a10703c279015bb
richardyao <richardyaoyxs@hotmail.com>
2016-03-25 23:24:35 +0800

    Initial commit

---
+++ app/src/main/java/yxs/usst/edu/cn/androidobj/MainActivity.java
@@ -0,0 +1,126 @@
+package yxs.usst.edu.cn.androidobj;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText userName;
+    EditText userPwd;
+    Button login;
+    Button clear;
+    boolean flag = false;
+    Button changeView;
+    TextView imgName;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userName = (EditText) findViewById(R.id.inputUserName);
+        userPwd = (EditText) findViewById(R.id.inputUserPwd);
+        userPwd.setLeft(userName.getLeft());
+
+        login = (Button) findViewById(R.id.loginBtn);
+        clear = (Button) findViewById(R.id.clearBtn);
+        changeView = (Button) findViewById(R.id.changeBtn);
+        imgName = (TextView) findViewById(R.id.imageName);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() > 0) {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+                login.setEnabled(flag);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        userPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() > 0) {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+                clear.setEnabled(flag);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        if(flag) {
+            login.setEnabled(true);
+            clear.setEnabled(true);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uName = userName.getText().toString().trim();
+                String uPwd = userPwd.getText().toString().trim();
+                TextView content = (TextView) findViewById(R.id.showContent);
+                content.setText("user name is" + uName + " and user passwd is " + uPwd);
+                Log.i("login", content.getText().toString());
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userName.setText("");
+                userPwd.setText("");
+                Log.i("clear", "clear username and password");
+            }
+        });
+
+        changeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = imgName.getText().toString();
+                if(name.equals("芙兰朵露")) {
+                    Drawable drawable = getResources().getDrawable(R.drawable.bilibili);
+                    imageView.setImageDrawable(drawable);
+                    imgName.setText("bilibili");
+                } else if(name.equals("bilibili")) {
+                    Drawable drawable = getResources().getDrawable(R.drawable.show);
+                    imageView.setImageDrawable(drawable);
+                    imgName.setText("芙兰朵露");
+                }
+            }
+        });
+
+    }
+}
